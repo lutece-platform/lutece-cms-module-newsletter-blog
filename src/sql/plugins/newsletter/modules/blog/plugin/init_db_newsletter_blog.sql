2903726814fc680aa0fe2ec422d7331b7b0449d2
TimotheeHrl <tharleaux@gmail.com>
2024-01-24 11:33:43 +0100

    LUT-27256 : Error inserting default template during db creation

--- src/sql/plugins/newsletter/modules/blog/plugin/init_db_newsletter_blog.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM newsletter_template WHERE id_template=2;
-INSERT INTO newsletter_template VALUES (2, 'Modèle de liste de billets - Blog', 'model_blogs.html', 'model-blog.svg', 'all', 'NEWSLETTER_BLOG', 1);
