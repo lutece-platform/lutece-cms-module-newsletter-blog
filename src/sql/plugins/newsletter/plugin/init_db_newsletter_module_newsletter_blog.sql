ff2187a5082b20ee91e9bbdeda51951cef562d53
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-09-23 09:06:19 +0200

    LUT-30782 : Prepare SQL scripts for Liquibase plugin

--- src/sql/plugins/newsletter/plugin/init_db_newsletter_module_newsletter_blog.sql
+++ src/sql/plugins/newsletter/plugin/init_db_newsletter_module_newsletter_blog.sql
@@ -1,2 +1,5 @@
+-- liquibase formatted sql
+-- changeset newsletter-blog:init_db_newsletter_module_newsletter_blog.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 DELETE FROM newsletter_template WHERE template_file_key= 'model_blogs.html';
 INSERT INTO newsletter_template(description, template_file_key, picture_file_key, workgroup_key, topic_type) VALUES ('Modèle de liste de billets - Blog', 'model_blogs.html', 'model-blog.svg', 'all', 'NEWSLETTER_BLOG');
